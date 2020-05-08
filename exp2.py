528563038fcaeeefbb694330629f61d3c9050bac
rajghate <rajghate@hotmail.com>
2020-05-08 15:56:32 +0530

    edited

--- exp2.py
+++ exp2.py
@@ -9,9 +9,12 @@ Created on Tue May  5 18:50:10 2020
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
 
-text="Nick likes to play football, however he is not too fond of tennis"
+textfile="text_english.txt"
+with open(textfile,"r") as file:
+    text=file.read()
+
 stopwords=list(stopwords.words('english'))
 words=word_tokenize(text)
 filtered_text=[w for w in words if not w in stopwords]
-print("original sentence:"+text)
-print("Stopword filtered sentence:"+(" ".join(filtered_text)))
+print("original text \n:"+text)
+print("Stopword filtered text \n:"+(" ".join(filtered_text)))
