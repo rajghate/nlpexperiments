6354b729ace0963f804540e98abe40d02854a41e
rajghate <rajghate@hotmail.com>
2020-05-12 14:23:38 +0530

    added

---
+++ exp5_regional.py
@@ -0,0 +1,45 @@
+from nltk.util import ngrams
+import pandas as pd
+from nltk.tokenize import word_tokenize
+import codecs
+
+with codecs.open("ngram_corpus_hindi.txt",encoding="utf-8") as file:
+    text=file.read()
+
+
+tokens=word_tokenize(text)
+bigram=list(ngrams(tokens,2))
+unigram=list(ngrams(tokens,1))
+dict_unigram={}
+for i in unigram:
+    if i not in dict_unigram.keys():
+        dict_unigram[i]=1
+    else:
+        dict_unigram[i]+=1
+
+dict_bigram={}
+for i in bigram:
+    if i not in dict_bigram.keys():
+        dict_bigram[i]=1
+    else:
+        dict_bigram[i]+=1
+
+dataunigram=[]
+for i in dict_unigram.keys():
+    dataunigram.append([i,dict_unigram[i]])
+
+databigram=[]
+for i in dict_bigram.keys():
+    databigram.append([i,dict_bigram[i]])
+    
+print(pd.DataFrame(dataunigram,columns=['Unigram','Frequency']))
+print(pd.DataFrame(databigram,columns=['Bigram','Frequency']))    
+
+sample_text="आपने हमें भारी समर्थन दिया है"
+
+bi_gram_test=list(ngrams(word_tokenize("> "+sample_text+" <"),2))
+pvalue=1
+for i in bi_gram_test:
+    pvalue *=dict_bigram[i]/text.count(bi_gram_test[0][1])
+    
+print("probability of the sentence :\"{}\" \naccording to the given corpus is :{}".format(sample_text,pvalue))
